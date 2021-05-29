a14b0532ecd4c1854501712aa9c50670be2259f0
Steve <hez2010@outlook.com>
2021-05-29 20:27:27 +0800

    Add workaround

--- Program.cs
+++ Program.cs
@@ -4,7 +4,6 @@
 using System;
 using System.IO;
 using System.Text.Json;
-using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
