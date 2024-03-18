5cbc4fd634c1b1488b3b9405212325214cc1cddb
Steve <hez2010@outlook.com>
2024-03-18 10:12:07 +0000

    Fix static build

--- build_libpcap_amd64.sh
+++ build_libpcap_amd64.sh
@@ -3,7 +3,6 @@
 wget https://www.tcpdump.org/release/libpcap-1.10.4.tar.xz
 tar xvf libpcap-1.10.4.tar.xz
 cd libpcap-1.10.4
-apt install flex bison
 ./configure --enable-remote --disable-universal
 make
 cd ..
